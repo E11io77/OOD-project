357abe4da0a4af9df293dd923823014d3613582a
E11io77 <elliott.knotek@gmail.com>
2026-05-07 09:44:49 +0200

    data loader to accept interface

---
+++ java-ood-project/app/src/main/java/com/myApp/domain/MaterialRepository.java
@@ -0,0 +1,10 @@
+package com.myApp.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MaterialRepository { //umožní udělat template, seznam "slibů"
+    List<Material> findAll();
+    Optional<Material> findByName(String name);
+    void addMaterial(Material material);
+}
---
+++ java-ood-project/app/src/main/java/com/myApp/domain/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.myApp.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+    Optional<Product> findByName(String name);
+    void addProduct(Product product);
+}
